76a0af1ecf1b2bf2c0588b79ec3278b176a8d6b1
Fernando Sepúlveda <fer.development03@gmail.com>
2025-05-10 08:33:31 -0400

    Clase 3

--- src/main/java/cl/fernando/CapaServicio/controller/AlumnoController.java
+++ src/main/java/cl/fernando/CapaServicio/controller/AlumnoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,10 +26,5 @@ public class AlumnoController {
     @GetMapping
     public List<Alumno> listar() {
         return alumnoService.listar();
-    }
-
-    @PostMapping("/{rut}/asignar/{sigla}")
-    public String alumnoAsignarCurso(@PathVariable String rut, @PathVariable String sigla) {
-        return alumnoService.alumnoAsignarCurso(rut, sigla);
     }
 }
--- src/main/java/cl/fernando/CapaServicio/service/AlumnoService.java
+++ src/main/java/cl/fernando/CapaServicio/service/AlumnoService.java
@@ -1,10 +1,11 @@
 package cl.fernando.CapaServicio.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cl.fernando.CapaServicio.model.Alumno;
-import cl.fernando.CapaServicio.model.Curso;
 import cl.fernando.CapaServicio.repository.AlumnoRepository;
 import cl.fernando.CapaServicio.repository.CursoRepository;
 
@@ -24,25 +25,9 @@ public class AlumnoService {
         return "Alumno registrado exitosamente.";
     }
 
-    public String alumnoAsignarCurso(String rut, String sigla) {
-        // OBTENEMOS ALUMNO Y CURSO
-        if(!cursoRepository.existsById(sigla)) {
-            return "El curso ingresado no fue encontrado";
-        }
-
-        if(!alumnoRepository.existsById(rut)) {
-            return "El alumno ingresado no fue encontrado";
-        }
-
-        Curso curso = cursoRepository.findById(sigla).get();
-        Alumno alumno = alumnoRepository.findById(rut).get();
-
-        if(alumno.getCursos().contains(curso)) {
-            return "El alumno ya se encuentra inscrito en el curso";
-        } else {
-            alumno.getCursos().add(curso);
-            alumnoRepository.save(alumno);
-            return "Alumno asignado al curso correctamente";
-        }
+    public List<Alumno> listar() {
+        return alumnoRepository.findAll();
     }
+
+    
 }
